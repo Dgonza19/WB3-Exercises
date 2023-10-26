7766eae6e01d924c9c4fb7b78f6866d3d5245311
Dgonza19 <Daisy.gu07@gmail.com>
2023-10-26 13:21:11 -0500

    Finished date_to_string.js

---
+++ Dates/Date Scripts/Scripts/date_to_string.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let currentDate = new Date("10/26/2023");
+
+
+console.log(currentDate.toString());
+console.log(currentDate.toDateString());
+console.log(currentDate.toUTCString());
+console.log(currentDate.toLocaleString());
+
+
+
