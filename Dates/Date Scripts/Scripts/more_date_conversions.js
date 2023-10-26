ac63a5ee746bdaa05b8d25c80313787b7a3a5eb3
Dgonza19 <Daisy.gu07@gmail.com>
2023-10-26 13:29:01 -0500

    Added more_date_conversions.js

---
+++ Dates/Date Scripts/Scripts/more_date_conversions.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let myBirthday = ("07/19/1995");
+let momsBirthday = ("12 21 1995");
+let dadsBirthday = new Date(1953, 0, 30);
+
+console.log(myBirthday.toLocaleString());
+console.log(momsBirthday.toLocaleString());
+console.log(dadsBirthday.toLocaleString());
